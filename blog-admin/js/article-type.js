add093676e0109ac7ea9fdd8cee56f6c93ecac1c
wu_ling <hxxian808@gmail.com>
2020-05-15 08:16:15 +0800

    完成文章类别新增接口与加载数据的后端对接

--- blog-admin/js/article-type.js
+++ blog-admin/js/article-type.js
@@ -1,23 +1,61 @@
 $(function() {
 	var vue = new Vue({
 		el: "#bloger",
+		data: {
+			typeName: '',
+			typeList: []
+		},
 		methods: {
-			addType: function() {
+			loadData: function() {
+				console.log("load data...")
+				let that = this;
+				$.ajax({
+					url: basePath + 'admin/article/type',
+					type: 'get',
+					dataType: 'json',
+					statusCode: {
+						200: function(data) {
+							console.log(data)
+							that.typeList = data;
+						}
+					}
+				})
+			},
+			showAddType: function() {
 				layer.open({
 					type: 1,
 					title: "新增分类",
-					offset:  '200px',
+					offset: '200px',
 					area: ['350px', '200px'],
 					content: addTypeHtml
 				});
+			},
+			saveType: function() {
+				console.log(123)
+				let that = this;
+				$.ajax({
+					url: basePath + 'admin/article/type',
+					type: 'post',
+					data: {
+						typeName: that.typeName
+					},
+					dataType: 'json',
+					statusCode: {
+						201: function() {
+							console.log("=======")
+						}
+					}
+				})
 			}
 		}
 	})
+	
+	vue.loadData();
 
 })
 
-var addTypeHtml = 
-	'<div class="add-type-box"> ' +
-	'	<input class="form-control" placeholder="分类名..." /> ' +
-	' 	<button class="btn btn-success">保存</button> ' +
-	'</div>';
+var addTypeHtml = '';
+// '<div class="add-type-box"> ' +
+// '	<input v-model="typeName" class="form-control" placeholder="分类名..." /> ' +
+// ' 	<button @click="saveType()" class="btn btn-success">保存</button> ' +
+// '</div>';
---
+++ blog-admin/js/common.js
@@ -0,0 +1,2 @@
+var DEBUG = true;
+var basePath = DEBUG ? "http://127.0.0.1:8181/" : "";
