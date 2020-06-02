9b1b12df252e50e2dc752a8ae6efbcb20d12cceb
wu_ling <hxxian808@gmail.com>
2020-06-02 08:36:33 +0800

    新增dashboard页数据查询处理

--- blog-admin/js/dashboard.js
+++ blog-admin/js/dashboard.js
@@ -2,12 +2,24 @@ $(function() {
 	let dashboard = new Vue({
 		el: '#dashboard',
 		data: {
-			
+			articleCount: 0,
+			accessCount: 0,
+			commentCount: 0,
+			foundingDays: 0
 		},
 		methods: {
 			loadData: function() {
-				
+				getSiteSnapshot().then((res) => {
+					if (res) {
+						this.articleCount = res.articleCount;
+						this.accessCount = res.accessCount;
+						this.commentCount = res.commentCount;
+						this.foundingDays = res.foundingDays;
+					}
+				})
 			}
 		}
 	})
+	
+	dashboard.loadData();
 })
